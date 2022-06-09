a92ab2024805cbe8bc94ef9e0cfa0a9fd9cee805
Maroua ZEBLAH <zeblahm@w.ensma.fr>
2022-06-09 10:51:24 +0200

    first commit

---
+++ ImportantConfig.py
@@ -0,0 +1,10 @@
+class Config:
+    def __init__(self,):
+        self.sytheticDir = "sytheticDir"
+        self.JOBDir = "JOB-queries"
+        self.schemaFile = "schema.sql"
+        self.dbName = "job2"
+        self.userName = "postgres"
+        self.password = "123"
+        self.ip = "172.0.0.1"
+        self.port = 5432
